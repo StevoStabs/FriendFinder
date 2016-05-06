3de52b8e47591c5ff46cb512f5fd2039354b4fef
Steven Stabile <stevenstabile@gmail.com>
2016-05-05 23:38:00 -0400

    finished assignment

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,12 @@
 var friends = [
 	{
 		"name": "Calvin",
-		"photo": "../public/img/calvin.jpg",
+		"photo": "https://s-media-cache-ak0.pinimg.com/736x/da/a5/d3/daa5d3b977b95f59a31e48ddfceabb8c.jpg",
 		"scores": [3, 2, 5, 1, 4, 2, 2, 4, 3, 1]
 	},
 	{
 		"name": "Hobbes",
-		"photo": "../public/img/hobbes.jpg",
+		"photo": "http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg",
 		"scores": [3, 2, 1, 5, 3, 4, 2, 1, 4, 3]
 	}
 ];
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -10,25 +10,50 @@ module.exports = function(app){
 
     app.post('/api/friends', function(req, res){
         
-        // loop through each of user's scores
-        // for(i=0; i<req.body.scores.length; i++){
 
-        // }
+        var counterArray = [];
+        var counter = 0
+        var user = req.body;
+        var bestFriend = {};
 
+        for(x=0; x<friends.length; x++){
 
-        
+            for(i=0; i<user.scores.length; i++) {
+                    
+                counter += Math.abs(user.scores[i] - friends[x].scores[i]);
+
+            };
+            counterArray.push(counter);
+            console.log(counter);
+            counter = 0;
+
+        };
+
+        console.log(counterArray);
+
+        var closestMatch = Math.min.apply(Math, counterArray);
+
+        console.log(closestMatch);
+         
+        for(x=0; x<friends.length; x++){
+
+            for(i=0; i<user.scores.length; i++) {
+                    
+                counter += Math.abs(user.scores[i] - friends[x].scores[i]);
+
+            };
+            if(counter === closestMatch) {
+                bestFriend = friends[x];
+            }
+            
+            counter = 0;
+
+        };
+              
         friends.push(req.body);
-        res.json(req.body);
+        res.json(bestFriend);
+        
 
 	});
 
 }
-
-
-
-
-// loop through each possible match
-// for(i=0; i<friends.length; i++){
-
-// 	friends[i].scores
-// }
